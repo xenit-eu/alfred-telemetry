6f0cf10539a91846796c1989b79c25ec743b76eb
Roxana Angheluta <anghelutar@github.com>
2020-11-15 19:28:16 +0100

    Unused imports.

--- alfred-telemetry-solr/solr4/src/main/java/eu/xenit/alfred/telemetry/solr/monitoring/binder/TomcatBeansMetrics.java
+++ alfred-telemetry-solr/solr4/src/main/java/eu/xenit/alfred/telemetry/solr/monitoring/binder/TomcatBeansMetrics.java
@@ -1,29 +1,17 @@
 package eu.xenit.alfred.telemetry.solr.monitoring.binder;
 
 import eu.xenit.alfred.telemetry.solr.util.JmxUtils;
-import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import javax.management.Attribute;
-import javax.management.AttributeList;
-import javax.management.AttributeNotFoundException;
-import javax.management.InstanceNotFoundException;
-import javax.management.IntrospectionException;
-import javax.management.MBeanAttributeInfo;
-import javax.management.MBeanException;
-import javax.management.MBeanInfo;
 import javax.management.MBeanServer;
-import javax.management.MalformedObjectNameException;
-import javax.management.ObjectName;
-import javax.management.ReflectionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/* Implemented because jmx metrics from micrometer - TomcatMetrics need a manager object which is not available */
+/***
+ *  Implemented because micrometer's default TomcatMetrics needs a manager object which is not available for Alfresco's solr
+ ***/
 public class TomcatBeansMetrics implements MeterBinder {
 
     MBeanServer mBeanServer;
