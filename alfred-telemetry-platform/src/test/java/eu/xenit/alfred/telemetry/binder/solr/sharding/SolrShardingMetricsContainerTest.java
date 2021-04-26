a531688bc683d8a1e2cdfa531a5e5200abfe3276
Daan Kerkhofs <daan.kerkhofs@xenit.eu>
2021-04-26 14:26:37 +0200

    Include SolrShardingMetricsContainerTest

---
+++ alfred-telemetry-platform/src/test/java/eu/xenit/alfred/telemetry/binder/solr/sharding/SolrShardingMetricsContainerTest.java
@@ -0,0 +1,43 @@
+package eu.xenit.alfred.telemetry.binder.solr.sharding;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import org.alfresco.repo.index.shard.Floc;
+import org.alfresco.repo.index.shard.Shard;
+import org.alfresco.repo.index.shard.ShardRegistry;
+import org.alfresco.repo.index.shard.ShardState;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class SolrShardingMetricsContainerTest {
+
+    @Mock
+    private ShardRegistry shardRegistry;
+
+    private SolrShardingMetricsContainer container;
+
+    @BeforeEach
+    void setup() {
+        container = new SolrShardingMetricsContainer(shardRegistry);
+    }
+
+    @Test
+    void refresh() {
+        Floc floc = new Floc();
+        HashMap<Floc, HashMap<Shard, HashSet<ShardState>>> flocs = new HashMap<>();
+        flocs.put(floc, new HashMap<>());
+        when(shardRegistry.getFlocs()).thenReturn(flocs);
+
+        container.refresh();
+        assertThat(container.getFlocs(), contains(floc));
+    }
+
+}
