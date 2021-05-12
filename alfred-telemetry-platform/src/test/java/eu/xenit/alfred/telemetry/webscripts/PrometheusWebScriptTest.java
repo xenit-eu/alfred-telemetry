5a0aaddd9ead087993a432c25f00f8f4087537ec
Daan Kerkhofs <daan.kerkhofs@xenit.eu>
2021-05-12 12:52:51 +0200

    gh-70 expand test coverage

--- alfred-telemetry-platform/src/test/java/eu/xenit/alfred/telemetry/webscripts/PrometheusWebScriptTest.java
+++ alfred-telemetry-platform/src/test/java/eu/xenit/alfred/telemetry/webscripts/PrometheusWebScriptTest.java
@@ -6,12 +6,17 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import eu.xenit.alfred.telemetry.registry.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -23,8 +28,25 @@ import org.springframework.extensions.webscripts.WebScriptResponse;
 @ExtendWith(MockitoExtension.class)
 class PrometheusWebScriptTest {
 
+    private static final String TEST_SCRAPE_TEXT = "dummy";
+
     @Mock
     private PrometheusMeterRegistry registry;
+
+    @Test
+    void execute() throws IOException {
+        PrometheusWebScript webScript = initWebScript(1);
+        when(registry.scrape()).thenReturn(TEST_SCRAPE_TEXT);
+
+        WebScriptRequest request = mock(WebScriptRequest.class);
+        WebScriptResponse response = mock(WebScriptResponse.class);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        when(response.getOutputStream()).thenReturn(out);
+
+        webScript.execute(request, response);
+        verify(response).setStatus(200);
+        assertThat(out.toString("UTF-8"), is(equalTo(TEST_SCRAPE_TEXT)));
+    }
 
     @Test
     void execute_maxRequestsReached() {
@@ -32,10 +54,10 @@ class PrometheusWebScriptTest {
 
         when(registry.scrape()).thenAnswer(answer -> {
             await().forever().until(() -> false);
-            return "dummy";
+            return TEST_SCRAPE_TEXT;
         });
 
-        Runnable runnable = () -> {
+        Runnable firstHttpRequest = () -> {
             WebScriptRequest request = mock(WebScriptRequest.class);
             WebScriptResponse response = mock(WebScriptResponse.class);
             try {
@@ -44,16 +66,20 @@ class PrometheusWebScriptTest {
                 throw new UncheckedIOException(e);
             }
         };
-        Thread thread = new Thread(runnable);
-        thread.start();
 
-        WebScriptRequest request = mock(WebScriptRequest.class);
-        WebScriptResponse response = mock(WebScriptResponse.class);
+        Runnable secondHttpRequest = () -> {
+            WebScriptRequest request = mock(WebScriptRequest.class);
+            WebScriptResponse response = mock(WebScriptResponse.class);
+            WebScriptException e = assertThrows(WebScriptException.class, () -> webScript.execute(request, response));
+            assertThat(e.getStatus(), is(equalTo(503)));
+        };
 
-        WebScriptException e = assertThrows(WebScriptException.class, () -> webScript.execute(request, response));
-        assertThat(e.getStatus(), is(equalTo(503)));
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(firstHttpRequest);
+        Future<?> future = executorService.submit(secondHttpRequest);
 
-        thread.interrupt();
+        await().until(future::isDone);
+        executorService.shutdownNow();
     }
 
     private PrometheusWebScript initWebScript(int maxRequests) {
