3d9d41d738ee68b88881302ade981313c69e46d1
Thijs Lemmens <thijs.lemmens@xenit.eu>
2021-06-16 14:25:22 +0200

    #107 Handle change of solrTrackingComponent to trackingComponent

--- alfred-telemetry-platform/src/main/java/eu/xenit/alfred/telemetry/binder/solr/SolrMetricsBeanPostProcessor.java
+++ alfred-telemetry-platform/src/main/java/eu/xenit/alfred/telemetry/binder/solr/SolrMetricsBeanPostProcessor.java
@@ -1,9 +1,10 @@
 package eu.xenit.alfred.telemetry.binder.solr;
 
-import eu.xenit.alfred.telemetry.binder.solr.sharding.SolrShardingMetrics;
 import eu.xenit.alfred.telemetry.binder.solr.sharding.SolrShardingMetricsFactory;
+import eu.xenit.alfred.telemetry.binder.solr.tracking.SolrTrackingMetrics;
 import io.micrometer.core.instrument.MeterRegistry;
 import java.util.Properties;
+import org.alfresco.service.transaction.TransactionService;
 import org.quartz.Scheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +21,10 @@ public class SolrMetricsBeanPostProcessor implements BeanDefinitionRegistryPostP
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SolrMetricsBeanPostProcessor.class);
 
+    public static final String SEARCH_TRACKING_COMPONENT_BEAN_ID_BEFORE_7 = "search.solrTrackingComponent";
+    public static final String SEARCH_TRACKING_COMPONENT_BEAN_ID_AFTER_7 = "search.trackingComponent";
+    public static final String SOLR_TRACKING_METRICS_ENABLED_PROPERTY = "alfred.telemetry.binder.solr.tracking.enabled";
+    public static final String SOLR_TRACKING_METRICS_BEAN_ID = "eu.xenit.alfred.telemetry.binder.solr.tracking.SolrTrackingMetrics";
     public static final String SOLR_SHARDING_REGISTRY_BEAN_ID = "search.SolrShardRegistry";
     public static final String SOLR_SHARDING_METRICS_BEAN_ID = "eu.xenit.alfred.telemetry.binder.solr.sharding.SolrShardingMetrics";
     public static final String SOLR_SHARDING_METRICS_CRON_PROPERTY = "alfred.telemetry.binder.solr.sharding.cronexpression";
@@ -30,15 +35,50 @@ public class SolrMetricsBeanPostProcessor implements BeanDefinitionRegistryPostP
     private final Properties globalProperties;
 
     private MeterRegistry meterRegistry;
+    private TransactionService transactionService;
 
-    public SolrMetricsBeanPostProcessor(Properties globalProperties, MeterRegistry meterRegistry, Scheduler scheduler) {
+    public SolrMetricsBeanPostProcessor(Properties globalProperties, MeterRegistry meterRegistry, Scheduler scheduler, TransactionService transactionService) {
         this.globalProperties = globalProperties;
         this.meterRegistry = meterRegistry;
         this.scheduler = scheduler;
+        this.transactionService = transactionService;
     }
 
     @Override
     public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry beanDefinitionRegistry) throws BeansException {
+        createTrackingMetricsBean(beanDefinitionRegistry);
+        createShardingMetricsBean(beanDefinitionRegistry);
+    }
+
+    private void createTrackingMetricsBean(BeanDefinitionRegistry beanDefinitionRegistry) {
+        if (!Boolean.parseBoolean(globalProperties.getProperty(SOLR_TRACKING_METRICS_ENABLED_PROPERTY))) {
+            LOGGER.info("Solr tracking metrics are not enabled, skipping.");
+            return;
+        }
+
+        BeanDefinition solrTrackingComponentBean;
+        try {
+            //Fetching bean for Alfresco <7
+            solrTrackingComponentBean = beanDefinitionRegistry.getBeanDefinition(
+                    SEARCH_TRACKING_COMPONENT_BEAN_ID_BEFORE_7);
+        } catch (NoSuchBeanDefinitionException e) {
+            LOGGER.info(String.format("%s not found, trying %s", SEARCH_TRACKING_COMPONENT_BEAN_ID_BEFORE_7,
+                    SEARCH_TRACKING_COMPONENT_BEAN_ID_AFTER_7));
+            solrTrackingComponentBean = beanDefinitionRegistry.getBeanDefinition(
+                    SEARCH_TRACKING_COMPONENT_BEAN_ID_AFTER_7);
+        }
+        GenericBeanDefinition solrTrackingMetricsBean = new GenericBeanDefinition();
+        solrTrackingMetricsBean.setBeanClass(SolrTrackingMetrics.class);
+        ConstructorArgumentValues constructorArgumentValues = new ConstructorArgumentValues();
+        constructorArgumentValues.addGenericArgumentValue(solrTrackingComponentBean);
+        constructorArgumentValues.addGenericArgumentValue(transactionService);
+        constructorArgumentValues.addGenericArgumentValue(meterRegistry);
+        solrTrackingMetricsBean.setConstructorArgumentValues(constructorArgumentValues);
+        beanDefinitionRegistry.registerBeanDefinition(SOLR_TRACKING_METRICS_BEAN_ID, solrTrackingMetricsBean);
+        LOGGER.info("Registered SolrTrackingMetrics bean");
+    }
+
+    private void createShardingMetricsBean(BeanDefinitionRegistry beanDefinitionRegistry) {
         if (!Boolean.parseBoolean(globalProperties.getProperty(SOLR_SHARDING_METRICS_ENABLED_PROPERTY))) {
             LOGGER.info("Solr sharding metrics are not enabled, skipping.");
             return;
--- alfred-telemetry-platform/src/main/java/eu/xenit/alfred/telemetry/binder/solr/tracking/SolrTrackingMetrics.java
+++ alfred-telemetry-platform/src/main/java/eu/xenit/alfred/telemetry/binder/solr/tracking/SolrTrackingMetrics.java
@@ -3,7 +3,7 @@ package eu.xenit.alfred.telemetry.binder.solr.tracking;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import java.util.function.ToDoubleFunction;
-import org.alfresco.repo.solr.SOLRTrackingComponent;
+import org.alfresco.repo.solr.SOLRTrackingComponentImpl;
 import org.alfresco.repo.transaction.RetryingTransactionHelper;
 import org.alfresco.service.transaction.TransactionService;
 import org.slf4j.Logger;
@@ -16,31 +16,28 @@ public class SolrTrackingMetrics {
     public static final String SOLR_METRICS_PREFIX = "solr.tracking";
     private TransactionService transactionService;
 
-    private SOLRTrackingComponent solrTrackingComponent;
+    private SOLRTrackingComponentImpl solrTrackingComponent;
     private MeterRegistry registry;
 
-    public SolrTrackingMetrics(SOLRTrackingComponent solrTrackingComponent, TransactionService transactionService,
-            MeterRegistry registry, boolean enabled) {
+    public SolrTrackingMetrics(SOLRTrackingComponentImpl solrTrackingComponent, TransactionService transactionService,
+            MeterRegistry registry) {
         this.solrTrackingComponent = solrTrackingComponent;
         this.transactionService = transactionService;
         this.registry = registry;
-        if (enabled) {
-            registerMetrics();
-        }
     }
 
     private void registerMetrics() {
         LOGGER.info("Registering Solr metrics");
-        registerSolrTrackingMetric("maxTxnId", SOLRTrackingComponent::getMaxTxnId, "number");
-        registerSolrTrackingMetric("maxTxnCommitTime", SOLRTrackingComponent::getMaxTxnCommitTime, "timestamp");
-        registerSolrTrackingMetric("maxChangeSetId", SOLRTrackingComponent::getMaxChangeSetId, "number");
-        registerSolrTrackingMetric("maxChangeSetCommitTime", SOLRTrackingComponent::getMaxChangeSetCommitTime,
+        registerSolrTrackingMetric("maxTxnId", SOLRTrackingComponentImpl::getMaxTxnId, "number");
+        registerSolrTrackingMetric("maxTxnCommitTime", SOLRTrackingComponentImpl::getMaxTxnCommitTime, "timestamp");
+        registerSolrTrackingMetric("maxChangeSetId", SOLRTrackingComponentImpl::getMaxChangeSetId, "number");
+        registerSolrTrackingMetric("maxChangeSetCommitTime", SOLRTrackingComponentImpl::getMaxChangeSetCommitTime,
                 "timestamp");
     }
 
-    private void registerSolrTrackingMetric(String name, ToDoubleFunction<SOLRTrackingComponent> function,
+    private void registerSolrTrackingMetric(String name, ToDoubleFunction<SOLRTrackingComponentImpl> function,
             String baseUnit) {
-        ToDoubleFunction<SOLRTrackingComponent> wrappedFunction = solrTrackingComponent1 -> {
+        ToDoubleFunction<SOLRTrackingComponentImpl> wrappedFunction = solrTrackingComponent1 -> {
             RetryingTransactionHelper retryingTransactionHelper = transactionService.getRetryingTransactionHelper();
             return retryingTransactionHelper
                     .doInTransaction(() -> function.applyAsDouble(solrTrackingComponent1), true);
