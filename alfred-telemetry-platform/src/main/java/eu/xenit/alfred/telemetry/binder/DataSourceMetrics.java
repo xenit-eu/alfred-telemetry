ea3cdc267565e12a7d260a63e8bbafdd1c45ba38
Zlatin Todorinski <zlatin.todorinski@xenit.eu>
2022-11-30 20:40:38 +0200

    DEVEM-515 Inherited directly Alfresco implementation rather than the parent Apache Commons class that broke with package renaming from dbcp to dbcp2 back in 2014 See https://commons.apache.org/proper/commons-dbcp/changes-report.html#a2.0

--- alfred-telemetry-platform/src/main/java/eu/xenit/alfred/telemetry/binder/DataSourceMetrics.java
+++ alfred-telemetry-platform/src/main/java/eu/xenit/alfred/telemetry/binder/DataSourceMetrics.java
@@ -5,7 +5,8 @@ import io.micrometer.core.instrument.MeterRegistry;
 import java.util.function.ToDoubleFunction;
 import javax.annotation.Nonnull;
 import javax.sql.DataSource;
-import org.apache.commons.dbcp.BasicDataSource;
+
+import org.alfresco.repo.tenant.TenantBasicDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,17 +19,18 @@ public class DataSourceMetrics implements NamedMeterBinder {
         return "jdbc";
     }
 
-    private final BasicDataSource dataSource;
+    private final TenantBasicDataSource dataSource;
 
     public DataSourceMetrics(DataSource dataSource) {
-        if (dataSource instanceof BasicDataSource) {
-            this.dataSource = (BasicDataSource) dataSource;
-        } else {
-            LOGGER.warn("Monitoring not supported for dataSource of type '{}', expected dataSource of type '{}'",
-                    dataSource.getClass().getCanonicalName(),
-                    BasicDataSource.class.getCanonicalName());
-            this.dataSource = null;
+        if (dataSource instanceof TenantBasicDataSource) {
+            this.dataSource = (TenantBasicDataSource) dataSource;
+            return;
         }
+
+        LOGGER.warn("Monitoring not supported for dataSource of type '{}', expected dataSource of type '{}'",
+                dataSource.getClass().getCanonicalName(),
+                TenantBasicDataSource.class.getCanonicalName());
+        this.dataSource = null;
     }
 
     @Override
@@ -37,34 +39,35 @@ public class DataSourceMetrics implements NamedMeterBinder {
             return;
         }
 
-        dataSourceGaugeBuilder("jdbc.connections.count", BasicDataSource::getNumActive)
+        dataSourceGaugeBuilder("jdbc.connections.count", TenantBasicDataSource::getNumActive)
                 .tags("status", "active")
                 .description("The current number of active connections that have been allocated from this data source.")
                 .register(meterRegistry);
-        dataSourceGaugeBuilder("jdbc.connections.count", BasicDataSource::getNumIdle)
+        dataSourceGaugeBuilder("jdbc.connections.count", TenantBasicDataSource::getNumIdle)
                 .tags("status", "idle")
                 .description(
                         "The current number of idle connections that are waiting to be allocated from this data source.")
                 .register(meterRegistry);
 
-        dataSourceGaugeBuilder("jdbc.connections.max", BasicDataSource::getMaxActive)
+        dataSourceGaugeBuilder("jdbc.connections.max", TenantBasicDataSource::getMaxActive)
                 .description("The maximum number of active connections that can be allocated at the same time.")
                 .register(meterRegistry);
 
-        dataSourceGaugeBuilder("jdbc.connections.min", BasicDataSource::getMinIdle)
+        dataSourceGaugeBuilder("jdbc.connections.min", TenantBasicDataSource::getMinIdle)
                 .description("The minimum number of idle connections in the pool.")
                 .register(meterRegistry);
 
-        dataSourceGaugeBuilder("jdbc.connections.usage", this::getUsage)
+        dataSourceGaugeBuilder("jdbc.connections.usage", DataSourceMetrics::getUsage)
                 .register(meterRegistry);
     }
 
-    private Gauge.Builder dataSourceGaugeBuilder(String metricName, ToDoubleFunction<BasicDataSource> function) {
+    private Gauge.Builder<TenantBasicDataSource> dataSourceGaugeBuilder(String metricName,
+                                                                    ToDoubleFunction<TenantBasicDataSource> function) {
         return Gauge.builder(metricName, dataSource, function)
                 .tags("name", "dbcp");
     }
 
-    private Float getUsage(BasicDataSource dataSource) {
+    private static Float getUsage(TenantBasicDataSource dataSource) {
         int maxSize = dataSource.getMaxActive();
         int currentSize = dataSource.getNumActive();
         if (maxSize < 0) {
